baf28f29d196f4872aed2f0227fda6395068010e
Aaron <33184112+astark929@users.noreply.github.com>
2026-07-14 16:10:22 -0700

    Update run_graph_pipeline.sh

--- solvingProblems/run_graph_pipeline.sh
+++ solvingProblems/run_graph_pipeline.sh
@@ -8,26 +8,35 @@
 # Change these paths
 # =========================
 
-RK_OUTPUT="RKoutput (1).csv"
-#EPSILON_WEIGHTS="epsilon_and_weights.txt"
-EPSILON_WEIGHTS="06-nu_e_coll-eps.csv"
+#RK_OUTPUT="RKoutput (1).csv"
+read -p "enter RKoutput file name: " RK_OUTPUT
+#this file must be inside the folder solvingProblems
+
+
+#EPSILON_WEIGHTS="06-nu_e_coll-eps.csv"
+read -p "enter EPSILON_WEIGHTS file name: " EPSILON_WEIGHTS
+#this file must be inside the folder solvingProblems
+
 
 #EXTRAP_HEADER="test_extrapolation.hh"
 
-RK_EXTRAP_ORIGINAL="RKextrap_original.csv"
-RK_EXTRAP="RKextrap.csv"
+#RK_EXTRAP_ORIGINAL="RKextrap_original.csv"
+read -p "enter RK_EXTRAP_ORIGINAL file name: " RK_EXTRAP_ORIGINAL
+#currently located in the local directory
+#aka inside the QKEsolveMPI-Main folder 
+
+#RK_EXTRAP="RKextrap.csv"
+read -p "enter RK_EXTRAP file name: " RK_EXTRAP
+#currently located in the local directory
+#aka inside the QKEsolveMPI-Main folder 
 
 # =========================
 # Compile C++ files
+#change the name of any files, if the names change
 # =========================
 
 echo "Compiling extrapolationCreation..."
-#g++ -Wall -Wextra -g3 extrapolationCreation.cpp -o extrapolationCreation.exe
 g++ ./solvingProblems/extrapolationCreation.cpp -std=c++17 -o extrapolationCreation.cpp
-
-# If test_RK_interpolation.sh already compiles run_RK_interp.cc,
-# you do not need this line.
-# g++ -Wall -Wextra -g3 run_RK_interp.cc -o run_RK_interp.exe
 
 
 # =========================
@@ -38,12 +47,14 @@ echo "Creating Test_extrapolation.hh..."
 ./extrapolationCreation.cpp "$RK_OUTPUT"
 # will take the rk output csv file to produce test_extrapolation.hh
 echo "extrapolationCreation.cpp finished"
+
 echo ""
 echo "if you see errors like, error on line 23, 35, and/or 41, do not worry."
 echo "the code is still running fine, however it just means it could not find a specific folder"
 echo "thus outputting data on the local directory"
 echo ""
 echo ""
+
 echo "Running RK interpolation script..." 
 "./script/test_RK_interpolation.sh" 
 # will take in or at least find the test_extrapolation.hh and 
