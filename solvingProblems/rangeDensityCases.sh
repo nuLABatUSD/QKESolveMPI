ed7170f7f4d22181c8236bc9665123f43bdb61bc
Aaron <33184112+astark929@users.noreply.github.com>
2026-07-24 14:10:52 -0700

    Update rangeDensityCases.sh

--- solvingProblems/rangeDensityCases.sh
+++ solvingProblems/rangeDensityCases.sh
@@ -1,4 +1,3 @@
-#C:/msys64/usr/bin/bash.exe
 #!/bin/sh
 
 
@@ -26,28 +25,6 @@ echo "creating file .... DensityRangeCases.hh"
 
 "$rangeexepath" "$@"
 
-: '
-#C:/msys64/usr/bin/bash.exe
-#!/bin/bash
-
-compiler="C:/msys64/mingw64/bin/g++.exe"
-
-source="./solvingProblems/rangeOfDensity.cpp"
-program="./solvingProblems/rangeOfDensity.exe"
-
-echo "Compiling..."
-
-"$compiler" "$source" -std=c++17 -O2 -o "$program"
-
-if [ $? -ne 0 ]; then
-    echo "Compilation failed."
-    exit 1
-fi
-
-echo "Running..."
-
-"$program" "$@"
-'
 #idk why there are multiple different types of commend such as ''' ''' and : ' ' 
 #   and << 'comment' ... comment
 #   but what ever works 
